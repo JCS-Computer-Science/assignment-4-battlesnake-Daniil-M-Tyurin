209dfd4dbc62dcd255833059f2969b9952e297a1
Daniil-M-Tyurin <daniilt3@educbe.ca>
2026-04-15 17:58:32 +0000

    Some stuff nothing important

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ server.get("/", (req, res) => {
 
 
 server.post("/start", (req, res) => {
-	console.log(req.body.board);
+	//console.log(req.body.board);
 	res.status(200).send();
 	
 })
@@ -35,12 +35,77 @@ server.post("/move", (req, res) => {
 		1, 1, 1, 1
 	]
 	//Snake randomized movement just cuz, mostly for testing
-	//possibleMoveArr = possibleMoveArr.map(value => value * (Math.random() * 0.9 + 0.1));
+	possibleMoveArr = possibleMoveArr.map(value => value * (Math.random() * 0.9 + 0.1));
 
 	//Create a rudimentary flood fill function
 
 	//Create a weighting that increases the desireability of food
+	if(yourself.health < 30){
+		let foodArr = board.food;
+		let distToFood = [];
+		let closestFoodIndex;
+		foodArr.forEach(food => {
+			let x = food.x;
+			let y = food.y;
+			let distance = Math.sqrt((yourself.head.x - x)**2 + (yourself.head.y - y)**2);
+			let pushedFood = {
+				x: x,
+				y: y,
+				distance: distance
+			}
+			distToFood.push(pushedFood);
+		})
+		//Figure this out
+		closestFoodIndex = distToFood.indexOf(Math.max(...distToFood.map(v => v.distance)));
+		let closestFood = distToFood[closestFoodIndex];
+		if (closestFood.x > yourself.head.x){
+			possibleMoveArr[1] = possibleMoveArr[1] * 10
+		}
+		if (closestFood.x < yourself.head.x){
+			possibleMoveArr[3] = possibleMoveArr[3] * 10
+		}
+		if (closestFood.y < yourself.head.y){
+			possibleMoveArr[2] = possibleMoveArr[2] * 10
+		}
+		if (closestFood.y > yourself.head.y){
+			possibleMoveArr[0] = possibleMoveArr[0] * 10
+		}
+		
+		
+	}
 
+	//Check current collision with head
+	snakesOthers.forEach(snake => {
+		let head = snake.head;
+		if (head.x === yourself.head.x && (head.y === yourself.head.y + 1)){
+			possibleMoveArr[0] = 0;
+		}
+		if (head.y === yourself.head.y && (head.x === yourself.head.x + 1)){
+			possibleMoveArr[1] = 0;
+		}
+		if (head.x === yourself.head.x && (head.y === yourself.head.y - 1)){
+			possibleMoveArr[2] = 0;
+		}
+		if (head.y === yourself.head.y && (head.x === yourself.head.x - 1)){
+			possibleMoveArr[3] = 0;
+		}
+	});
+	//Check future collisions witg geads
+	snakesOthers.forEach(snake => {
+		let head = snake.head;
+		if (head.x === yourself.head.x && head.y - 1 === yourself.head.y + 1){
+			possibleMoveArr[0] = possibleMoveArr[0] * .25;
+		}
+		if (head.y === yourself.head.y && (head.x -1  === yourself.head.x + 1)){
+			possibleMoveArr[1] = possibleMoveArr[1] * .25;
+		}
+		if (head.x === yourself.head.x && (head.y + 1 === yourself.head.y - 1)){
+			possibleMoveArr[2] = possibleMoveArr[2] * .25;
+		}
+		if (head.y === yourself.head.y && (head.x + 1 === yourself.head.x - 1)){
+			possibleMoveArr[3] = possibleMoveArr[3] * .25;
+		}
+	});
 	//check collision with wals
 	if(yourself.head.y + 1 >= board.height){
 		possibleMoveArr[0] = 0;
@@ -106,7 +171,7 @@ server.post("/move", (req, res) => {
 		"move": chosenMove
 	}
 	res.status(200).send(move);
-	console.log(move);
+	//console.log(move);
 })
 
 
