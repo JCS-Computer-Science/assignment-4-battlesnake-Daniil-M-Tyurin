aec4132addbafce3c9573c9b89910b998a7cc5b0
Daniil-M-Tyurin <daniilt3@educbe.ca>
2026-04-29 04:01:20 +0000

    Flood fill

--- index.js
+++ index.js
@@ -26,6 +26,9 @@ server.post("/start", (req, res) => {
 
 
 server.post("/move", (req, res) => {
+
+	//New code based on a heat map 
+
 	let board = req.body.board
 	let snakesArr = board.snakes
 	let yourself = req.body.you
@@ -53,17 +56,17 @@ server.post("/move", (req, res) => {
 	let xFood = foodArr[minFoodIndex].x
 	let yFood = foodArr[minFoodIndex].y
 	if (yourself.head.y < yFood){
-		possibleMoveArr[0] = possibleMoveArr[0] * 3
+		possibleMoveArr[0] = possibleMoveArr[0] * 10
 	}
 	if (yourself.head.y > yFood){
-		possibleMoveArr[2] = possibleMoveArr[2] * 3
+		possibleMoveArr[2] = possibleMoveArr[2] * 10
 	}
 
 	if (yourself.head.x < xFood){
-		possibleMoveArr[1] = possibleMoveArr[1] * 3
+		possibleMoveArr[1] = possibleMoveArr[1] * 10
 	}
 	if (yourself.head.x > xFood){
-		possibleMoveArr[3] = possibleMoveArr[3] * 3
+		possibleMoveArr[3] = possibleMoveArr[3] * 10
 	}
 	//Check current collision with head
 	snakesOthers.forEach(snake => {
@@ -83,51 +86,35 @@ server.post("/move", (req, res) => {
 
 		if (yourself.length >= snake.length + 1){
 			if((head.x === yourself.head.x && (head.y - 1 === yourself.head.y + 1)) || (head.y === yourself.head.y + 1 && (head.x + 1 === yourself.head.x)) || (head.y === yourself.head.y + 1 && (head.x - 1 === yourself.head.x))){
-				possibleMoveArr[0] = possibleMoveArr[0] * 3
+				possibleMoveArr[0] = possibleMoveArr[0] * 5
 			}
 			if((head.y - 1 === yourself.head.y && (yourself.head.x + 1 === head.x)) || (yourself.head.x + 1 === head.x - 1 && (yourself.head.y === head.y)) || (yourself.head.y === head.y + 1 && (yourself.head.x + 1 === head.x))){
-				possibleMoveArr[1] = possibleMoveArr[1] * 3
+				possibleMoveArr[1] = possibleMoveArr[1] * 5
 			}
 			if((yourself.head.x === head.x + 1 && (yourself.head.y - 1 === head.y)) || (yourself.head.x === head.x && (yourself.head.y - 1 === head.y + 1)) || (yourself.head.x === head.x - 1 && (yourself.head.y - 1 === head.y))){
-				possibleMoveArr[2] = possibleMoveArr[2] * 3
+				possibleMoveArr[2] = possibleMoveArr[2] * 5
 			}
 			if((yourself.head.x - 1 === head.x && (yourself.head.y === head.y - 1)) || (yourself.head.x - 1 === head.x + 1 && (yourself.head.y === head.y)) || (yourself.head.y === head.y + 1 && (yourself.head.x - 1 === head.x))){
-				possibleMoveArr[3] = possibleMoveArr[3] * 3
+				possibleMoveArr[3] = possibleMoveArr[3] * 5
 			}
 		} else {
 			if((head.x === yourself.head.x && (head.y - 1 === yourself.head.y + 1)) || (head.y === yourself.head.y + 1 && (head.x + 1 === yourself.head.x)) || (head.y === yourself.head.y + 1 && (head.x - 1 === yourself.head.x))){
-				possibleMoveArr[0] = 0
+				possibleMoveArr[0] = possibleMoveArr[0] * .005
 			}
 			if((head.y - 1 === yourself.head.y && (yourself.head.x + 1 === head.x)) || (yourself.head.x + 1 === head.x - 1 && (yourself.head.y === head.y)) || (yourself.head.y === head.y + 1 && (yourself.head.x + 1 === head.x))){
-				possibleMoveArr[1] = 0
+				possibleMoveArr[1] = possibleMoveArr[1] * .005
 			}
 			if((yourself.head.x === head.x + 1 && (yourself.head.y - 1 === head.y)) || (yourself.head.x === head.x && (yourself.head.y - 1 === head.y + 1)) || (yourself.head.x === head.x - 1 && (yourself.head.y - 1 === head.y))){
-				possibleMoveArr[2] = 0
+				possibleMoveArr[2] = possibleMoveArr[2] * .005
 			}
 			if((yourself.head.x - 1 === head.x && (yourself.head.y === head.y - 1)) || (yourself.head.x - 1 === head.x + 1 && (yourself.head.y === head.y)) || (yourself.head.y === head.y + 1 && (yourself.head.x - 1 === head.x))){
-				possibleMoveArr[3] = 0
+				possibleMoveArr[3] = possibleMoveArr[3] * .005
 			}
 
 		}
 
 
 	});
-
-	
-	//Check future collisions using recursion
-	
-
-
-
-
-
-
-
-
-
-
-
-
 	
 	//check collision with wals
 	if(yourself.head.y + 1 >= board.height){
@@ -203,6 +190,7 @@ server.post("/move", (req, res) => {
 	if (yourself.body.some(bodyElem => bodyElem.x === yourself.head.x - 1 && bodyElem.y === yourself.head.y)){
 		possibleMoveArr[3] = 0;
 	}
+
 	//Checks for two tunnel look ahead weighting of only one to be lowered
 
 	let directions = [
@@ -210,36 +198,106 @@ server.post("/move", (req, res) => {
 		{x: yourself.head.x + 1, y: yourself.head.y},
 		{x: yourself.head.x, y: yourself.head.y - 1},
 		{x: yourself.head.x - 1, y: yourself.head.y}
-	]
+	];
+	// flood fill insert? Dont know how well it works
+	//console.log(possibleMoveArr);
+
+
+	function lookAhead(start, snakesArr, board){
+		let visit = [];
+		let count = 0;
+		let needed = [start];
+		//console.log(needed.length)
+		while(needed.length > 0){
+			let playing = needed.shift();
+			console.log(playing)
+			if(playing.x < 0 || playing.x >= board.width || playing.y < 0 || playing.y >= board.height){
+				continue;
+			}
+			if(visit.some(visited => visited.x === playing.x && visited.y === playing.y)){
+				continue;
+			}
+			let isEdge = (playing.x === start.x && playing.y === start.y);
+			if(!isEdge && snakesArr.some(snake => snake.body.some(body => body.x === playing.x && body.y === playing.y))){
+				continue;
+			}
+			visit.push(playing);
+			count = count + 1;
+			needed.push({x: playing.x, y: playing.y + 1});
+			needed.push({x: playing.x + 1, y: playing.y});
+			needed.push({x: playing.x, y: playing.y - 1});
+			needed.push({x: playing.x - 1, y: playing.y});
+		}
+		//console.log(count)
+		return count;
+	}
+
+
+
+
+
+
+
 	directions.forEach((direct, i) => {
 		if (possibleMoveArr[i] === 0){
 			return;
 		}
-		let available = 0;
-		if(direct.y + 1 < board.height && !snakesArr.some(snake => snake.body.some(body => body.x === direct.x && body.y === direct.y + 1))){
-			available = available + 1;
-		}
+		let copySnakeArr = snakesArr.map(snake =>{
+			return{
+				id:snake.id,
+				body: snake.body.slice(),
+			}
+		});
+		let me = copySnakeArr.find(snake => snake.id === yourself.id);
+		me.body.unshift({x: direct.x, y: direct.y});
+		me.body.pop();
+		copySnakeArr.forEach(snake => {
+			snake.body.pop();
+		});
+		let spaceValue = lookAhead(direct, copySnakeArr, board);
+		//console.log(spaceValue)
 
-		if(direct.x + 1 < board.width && !snakesArr.some(snake => snake.body.some(body => body.x === direct.x + 1&& body.y === direct.y))){
-			available = available + 1;
+		if (spaceValue < yourself.length){
+			possibleMoveArr[i] = possibleMoveArr[i] * 0.0001;
+			return;
 		}
+		possibleMoveArr[i] = possibleMoveArr[i] * spaceValue;
+		//console.log("spaceValue:", spaceValue);
+	});
 
-		if(direct.y - 1 >= 0 && !snakesArr.some(snake => snake.body.some(body => body.x === direct.x && body.y === direct.y - 1))){
-			available = available + 1;
-		}
 
-		if(direct.x - 1 >= 0 && !snakesArr.some(snake => snake.body.some(body => body.x === direct.x - 1 && body.y === direct.y))){
-			available = available + 1;
-		}
 
-		if(available === 0){
-			possibleMoveArr[i] = 0;
-		} else if (available === 1) {
-			possibleMoveArr[i] = possibleMoveArr[i] * 0.001
-		} else if (available === 2) {
-			possibleMoveArr[i] = possibleMoveArr[i] * 0.002
-		}
-	})
+
+
+
+		// if (possibleMoveArr[i] === 0){
+		// 	return;
+		// }
+		// let available = 0;
+		// if(direct.y + 1 < board.height && !snakesArr.some(snake => snake.body.some(body => body.x === direct.x && body.y === direct.y + 1))){
+		// 	available = available + 1;
+		// }
+
+		// if(direct.x + 1 < board.width && !snakesArr.some(snake => snake.body.some(body => body.x === direct.x + 1&& body.y === direct.y))){
+		// 	available = available + 1;
+		// }
+
+		// if(direct.y - 1 >= 0 && !snakesArr.some(snake => snake.body.some(body => body.x === direct.x && body.y === direct.y - 1))){
+		// 	available = available + 1;
+		// }
+
+		// if(direct.x - 1 >= 0 && !snakesArr.some(snake => snake.body.some(body => body.x === direct.x - 1 && body.y === direct.y))){
+		// 	available = available + 1;
+		// }
+
+		// if(available === 0){
+		// 	possibleMoveArr[i] = 0;
+		// } else if (available === 1) {
+		// 	possibleMoveArr[i] = possibleMoveArr[i] * 0.01
+		// } else if (available === 2) {
+		// 	possibleMoveArr[i] = possibleMoveArr[i] * 0.02
+		// }
+
 
 
 
@@ -260,13 +318,14 @@ server.post("/move", (req, res) => {
 	if (moveIndex === 3){
 		chosenMove = "left"
 	}
-	console.log(board);
+	//console.log(board);
+	console.log(possibleMoveArr);
 	let move = {
 		"move": chosenMove
 	}
 	res.status(200).send(move);
 	//console.log(move);
-})
+});
 
 
 server.post("/end", (req, res) => {
